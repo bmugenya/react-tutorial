1e27cab2ba007b1c458b1ca24cb3a601802a0a74
Bmugenya <bmugenya26@gmail.com>
2021-02-11 12:06:34 +0300

    useState object

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import Setup from './tutorial/useState/setup/useState-array'
+import Setup from './tutorial/useState/setup/useState-object'
 
 function App() {
   return (
--- react-advanced/src/tutorial/useState/setup/useState-object.js
+++ react-advanced/src/tutorial/useState/setup/useState-object.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <div>UseState object</div>
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  })
+
+  const changeMessage = () => {
+    setPerson({ ...person, message: 'hello world' })
+  }
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        Change Message
+      </button>
+    </div>
+  )
 }
 
 export default UseStateObject
